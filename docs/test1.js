8da9312716d137a1d8d9a2085635272f91c3490b
Sebastián Gurin <sebastigurin@gmail.com>
2018-09-20 13:44:58 -0300

    test1

--- docs/test1.js
+++ docs/test1.js
@@ -4,13 +4,15 @@ import * as Magick from './magickApi.js'
 const images = [
   { sourceUrl: 'rotate.png', targetId: 'outputImage1', outFile: 'rotateOut.png' },
   { sourceUrl: 'Hermitcrab.gif', targetId: 'outputImage3', outFile: 'HermitcrabOut.gif' },
-  // { sourceUrl: 'gnu.jpg', targetId: 'outputImage2', outFile: 'out.jpg' }, doesn't work see https://github.com/KnicKnic/WASM-ImageMagick/issues/8
-  // { sourceUrl: 'react.svg', targetId: 'outputImage4', outFile: 'out.png' },// doesn't work
+  { sourceUrl: 'zelda.gif', targetId: 'outputImage5', outFile: 'zeldaOut.gif' },
+
+  { sourceUrl: 'gnu.jpg', targetId: 'outputImage2', outFile: 'out.jpg' }, // doesn't work see https://github.com/KnicKnic/WASM-ImageMagick/issues/8
+  { sourceUrl: 'react.svg', targetId: 'outputImage4', outFile: 'out.png' },// doesn't work
+
 ]
 
-async function DoMagickCall () {
+async function DoMagickCall() {
   images.forEach(async image => {
-
     let outputImage = document.getElementById(image.targetId)
 
     let fetchedSourceImage = await fetch(image.sourceUrl)
@@ -19,6 +21,8 @@ async function DoMagickCall () {
 
     const imArguments = JSON.parse(document.querySelector('.input').value)
       .map(a => a === '$INPUT' ? image.sourceUrl : a === '$OUTPUT' ? image.outFile : a)
+
+    document.querySelector('.im-command').innerHTML = arrayToIMCommand(imArguments)
 
     const files = [{ 'name': image.sourceUrl, 'content': sourceBytes }]
     const t0 = performance.now()
@@ -31,10 +35,16 @@ async function DoMagickCall () {
     outputImage.src = URL.createObjectURL(firstOutputImage['blob'])
     outputImage.setAttribute('data-outfile', image.outFile)
 
-    outputImage.parentElement.parentElement.querySelector('.took').innerHTML= 'Took: '+Math.round(took)+' ms'
-
+    outputImage.parentElement.parentElement.querySelector('.took').innerHTML = 'Took: ' + Math.round(took) + ' ms'
   })
+}
 
+function arrayToIMCommand(command) {
+  return command
+    .map(c => c.match(/\s/) ? `'${c}'` : c)
+    .map(c => c.replace(/\(/gm, '\\('))
+    .map(c => c.replace(/\)/gm, '\\)'))
+    .join(' ')
 }
 
 export function start() {
