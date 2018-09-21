392ddbae11fc562dcaa77cf46e371e30ab668490
Sebastián Gurin <sebastigurin@gmail.com>
2018-09-21 17:10:07 -0300

    convertDemo more transformations

--- src/ui/page/convertDemo/convertDemo.tsx
+++ src/ui/page/convertDemo/convertDemo.tsx
@@ -12,19 +12,21 @@ const styles = (theme: Theme) => createStyles({
     display: 'flex',
     flexWrap: 'wrap',
   },
+  select : {
+    minWidth: '240px'
+  },
   input: {
-
     width: '100%',
     height: '100px'
   },
+  demoEntry: {
+    border: '2px solid #ededed',
+    padding: '10px'
+  },
   images: {
-    td: {
-      border: '2px solid #ededed',
-      padding: '10px'
-    }
+
   },
   error: {
-
     margin: 0,
     padding: 0,
     fontWeight: 'bold'
@@ -42,21 +44,20 @@ function render(props: WithStyles<typeof styles>) {
     <Paper className={classes.paper}>
       <p>Try an ImageMagick command on different test images. Check out the suggestions!</p>
 
+      {renderSuggestions(props, transformations)}<br/><br/>
+
       <textarea className={classes.input + ' input'} defaultValue='["convert", "$INPUT", "-sigmoidal-contrast", "15x30%", "(", "+clone",  "-sparse-color", "Barycentric", "0,0 black 0,%h gray80", "-solarize", "50%", "-level", "50%,0", ")", "-compose", "Blur", "-set", "option:compose:args", "10", "-composite", "$OUTPUT"]'></textarea>
 
-      <p className="error">
-      </p>
+      <p className="error"></p>
+
+      <Button className="execute" variant="contained" onClick={() => transformImages()}>Execute</Button>
 
       <p>
         <strong>ImageMagick Command: </strong>
         <br />
         <span className="im-command"></span>
       </p>
-
-      {renderSuggestions(props)}
-      <br />
-      <br />
-      <Button className="execute" variant="contained" onClick={() => transformImages()}>Execute</Button>
+      {renderSuggestions(props, suggestionsDontWork, 'Not working transformations')}
 
       {renderImageTable(props)}
 
@@ -64,25 +65,22 @@ function render(props: WithStyles<typeof styles>) {
   )
 }
 
-// function handleChange(){
-//   debugger
-// }
-function renderSuggestions(props: WithStyles<typeof styles>) {
+function renderSuggestions(props: WithStyles<typeof styles>, transformations: ConvertDemoTransformation[], title: string='Transformation examples') {
   const { classes } = props
   return (<div>
     <form className={classes.root} autoComplete="off">
       <FormControl className={classes.formControl}>
-        <InputLabel htmlFor="age-simple">Suggestions</InputLabel>
-        <Select
-          value={transformations[0].command}
+        <InputLabel htmlFor="suggestion-simple">{title}</InputLabel>
+        <Select className={classes.select}
+          // value={transformations[0].command}
           onChange={suggestionChange}
           inputProps={{
-            name: 'age',
-            id: 'age-simple',
+            name: 'suggestion',
+            id: 'suggestion-simple',
           }}
         >
-          {transformations.map(t =>
-            <MenuItem value={t.command}>{t.name}</MenuItem>
+          {transformations.map((t: ConvertDemoTransformation, i: number) =>
+            <MenuItem   value={t.command}>{t.name}</MenuItem>
           )}
         </Select>
       </FormControl>
@@ -101,7 +99,7 @@ function renderImageTable(props: WithStyles<typeof styles>) {
       <table className={classes.images + ' images'}>
         <tbody>
           {defaultImages.map(image =>
-            <tr>
+            <tr className={classes.demoEntry}>
               <td>
                 <img src={image.sourceUrl} /><br />{image.sourceUrl}
               </td>
@@ -126,9 +124,21 @@ export default withStyles(styles)(render)
 
 
 
-import { images as defaultImages, ConvertDemoImage, transformations } from './data'
+import { images as defaultImages, transformations, ConvertDemoTransformation, suggestionsDontWork } from './data'
 import { buildImArguments, DoMagickCall, arrayToIMCommand } from './index';
-// import { images } from '../../../static/test1';
+
+
+function suggestionChange(e: React.ChangeEvent) {
+  const value: any = (e.target as any).value
+  document.querySelector<HTMLInputElement>('.input').value = JSON.stringify(value)
+  // selectedTransformationIndex = transformations.indexOf(transformations.find(t => JSON.stringify(t.command) === JSON.stringify(value)))
+  // debugger
+  transformImages()
+}
+
+function spinner(spinning: boolean, el: HTMLElement) {
+  (el.parentElement.parentElement.querySelector('.spinner') as HTMLElement).style.display = spinning ? 'block' : 'none'
+}
 
 async function transformImages(images = defaultImages) {
   images.forEach(async image => {
@@ -153,27 +163,4 @@ async function transformImages(images = defaultImages) {
 
     spinner(false, outputImage)
   })
-}
-
-
-// export function start() {
-// document.querySelector('.execute').addEventListener('click', e => {
-//   transformImages()
-// })
-// document.querySelector('.suggestions').addEventListener('change', e => {
-//   (document.querySelector('.input') as HTMLInputElement).value = [...(document.querySelectorAll('.suggestions option') as any)].find(e => e.selected).innerHTML
-//   transformImages()
-// })
-// transformImages()
-// }
-function suggestionChange(e: React.ChangeEvent) {
-
-  (document.querySelector('.input') as HTMLInputElement).value = JSON.stringify((e.target as any).value)
-  // (e.target as any).value.join(' ')//[...(document.querySelectorAll('.suggestions option') as any)].find(e => e.selected).innerHTML
-  transformImages()
-}
-
-
-function spinner(spinning: boolean, el: HTMLElement) {
-  (el.parentElement.parentElement.querySelector('.spinner') as HTMLElement).style.display = spinning ? 'block' : 'none'
 }
--- src/ui/page/convertDemo/data.ts
+++ src/ui/page/convertDemo/data.ts
@@ -13,7 +13,7 @@ export const images: ConvertDemoImage[] = [
 ]
 
 export interface ConvertDemoTransformation {
-  id: string, name: string, command: string[]
+  id: string, name: string, command: string[], description?: string
 }
 export const transformations: ConvertDemoTransformation[] = [
   {
@@ -47,59 +47,48 @@ export const transformations: ConvertDemoTransformation[] = [
     name: 'colorspaceChannelSeparate1',
     command: ["convert", "$INPUT", "-colorspace", "HSL", "-channel", "Hue", "-separate", "$OUTPUT"]
   },
+  {
+    id: 'charcoal1',
+    name: 'charcoal 1',
+    command: ["convert", "$INPUT", "-charcoal", "5", "$OUTPUT"],
+    description: 'The Charcoal Sketch Transform, offers users a very simple way of generating a simplified gray-scale rendering of the image.'
+  },
+  {
+    id: 'childrenCanColor1',
+    name: 'children can color 1',
+    command: ["convert", "$INPUT", "-edge", "1", "-negate", "-normalize", "-colorspace", "Gray", "-blur", "0x.5", "-contrast-stretch", "0x50%", "$OUTPUT"],
+    description: 'Convert a simple photo into something children can color in. '
+  },
+  {
+    id: 'childrenCanColor2',
+    name: 'children can color 2',
+    command: ["convert", "$INPUT", "-segment", "1x1", "+dither", "-colors", "2", "-edge", "1", "-negate", "-normalize", "$OUTPUT"],
+    description: 'Convert a simple photo into something children can color in. Shaded complex pictures'
+  },
 ]
 
+export const suggestionsDontWork = [
 
+  {
+    id: 'notWorking3',
+    name: 'notWorking3',
+    command: ["convert", "$INPUT", "-distort", "SRT", "%[fx:aa=$angle*pi/180;(w*abs(sin(aa))+h*abs(cos(aa)))/min(w,h)]", "$angle", "$OUTPUT"],
+    description: 'notWorking3'
+  },
+  {
+    id: 'notWorking2',
+    name: 'notWorking2',
+    command: ["convert", "$INPUT", "-size", "320x100", "xc:lightblue", "-font", "Candice", "-pointsize", "72", "-tile", "pattern:checkerboard", "-annotate", "+28+68", "Sebastian", "$OUTPUT"],
+    description: 'notWorking2'
+  },
+  {
+    id: 'notWorking1',
+    name: 'notWorking as expected',
+    command: ["convert", "$INPUT", "(", "+clone", "-scale", "25%", "-scale", "400%", ")", "(", "+clone", "-gamma", "0", "-fill", "white", "-draw", "circle 65,53 50,40", "-blur", "10x4", ")", "-composite", "$OUTPUT"],
+    description: 'NOT WORKING AS EXPECTED'
+  },
+]
 
-// <p>Suggestions: </p>
-// <select className="suggestions" onChange={suggestionChange}>
-
-// {transformations.map(t=>
-//   <option data-transformation-id={t.id}>{t.name}</option>
-// )}
-
-//   <option>["convert", "$INPUT", "-distort", "barrel", "0.7 -0.009 0.0", "-resize", "200%", "$OUTPUT"]</option>
-
-//   <option>["convert", "$INPUT", "-rotate", "90","-resize","200%", "$OUTPUT"]</option>
-
-//   <option>["convert", "$INPUT", "-sigmoidal-contrast", "4,0%", "$OUTPUT"]</option>
-
-//   <option>["convert","$INPUT", "-sparse-color", "Barycentric", "0,0 black 0,%h white", "-function", "polynomial",
-// "4,-4,1", "$OUTPUT"]</option>
-
-//   <option>["convert", "$INPUT", "-sigmoidal-contrast", "15x30%", "(", "+clone", "-sparse-color", "Barycentric", "0,0
-//     black 0,%h gray80", "-solarize", "50%", "-level", "50%,0", ")", "-compose", "Blur", "-set",
-// "option:compose:args", "10", "-composite", "$OUTPUT"]</option>
-
-//   {/* <!-- extracting the 'Hue' by Separating Channel Images, then looking up the 'hue shade' wanted. --> */}
-//   <option>["convert", "$INPUT", "-colorspace", "HSL", "-channel", "Hue", "-separate", "$OUTPUT"]</option>
-
-//   <option>["convert", "$INPUT", "-charcoal", "5", "$OUTPUT"]</option>
-//   {/* <!-- The Charcoal Sketch Transform, offers users a very simple way of generating a simplified gray-scale rendering of the image. --> */}
-
-//   {/* <!-- Convert a simple photo into something children can color in. --> */}
-//   <option>["convert", "$INPUT", "-edge", "1", "-negate", "-normalize", "-colorspace", "Gray", "-blur", "0x.5", "-contrast-stretch", "0x50%", "$OUTPUT"]</option>
-
-//   {/* <!-- Convert a simple photo into something children can color in. --> */}
-//   <option>["convert", "$INPUT", "-segment", "1x1", "+dither", "-colors", "2", "-edge", "1", "-negate", "-normalize", "$OUTPUT"]</option>
-
-//   {/* <!-- <option>["convert", "$INPUT", "-charcoal", "5", "$OUTPUT"]</option> -->
-// <!-- ", "-alpha", "set", "-background", "none", -vignette", "0x4"
-// ", "  --> */}
-//   <option>...</option>
-//   <option>THE FOLLOWING ONES DON'T WORK:</option>
-//   <option>...</option>
-
-//   <option>NOT WORKING AS EXPECTED: ["convert", "$INPUT", "(", "+clone", "-scale", "25%", "-scale", "400%", ")", "("
-//     ,"+clone", "-gamma", "0",
-// "-fill", "white", "-draw", "circle 65,53 50,40", "-blur", "10x4", ")", "-composite", "$OUTPUT"]</option>
-
-//   <option>NOT WORKING: see https://github.com/KnicKnic/WASM-ImageMagick/issues/9
-//     ["convert", "-size", "320x100", "xc:lightblue", "-font", "Candice", "-pointsize", "72", "-tile",
-//     "pattern:checkerboard", "-annotate", "+28+68", "Sebastian", "$OUTPUT"]
-// </option>
-//   {/* <!-- doesn't work  --> */}
-//   ",
 
 // <option>NOT WORKING: taken from https://imagemagick.org/Usage//photos/
 //                                 ["convert", "$INPUT", "-distort", "SRT", "%[fx:aa=$angle*pi/180;(w*abs(sin(aa))+h*abs(cos(aa)))/min(w,h)]",
