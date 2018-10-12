f5fd2469e4426f6edd8cf15014b1a71005112b31
Sebastian Gurin <sebastigurin@gmail.com>
2018-10-12 12:47:10 -0300

    im-browser tests

--- imagemagick-browser/src/test/withBrowser.test.ts
+++ imagemagick-browser/src/test/withBrowser.test.ts
@@ -1,16 +1,19 @@
 import * as puppeteer from 'puppeteer'
+import * as im from '../'
+type IM = typeof im
 
 describe('1', () => {
+
   let browser: puppeteer.Browser
   let page: puppeteer.Page
-
   beforeEach(async done => {
     browser = await puppeteer.launch({ args: ['--no-sandbox'] })
     page = await browser.newPage()
-    const url = `http://127.0.0.1:8080/`
-    await page.goto(url)
+    await page.goto(`http://127.0.0.1:8080/`)
+    await page.waitFor(() => { return (window as any).imageMagickBrowser })
     done()
   })
+
   afterEach(async done => {
     await browser.disconnect()
     done()
@@ -23,29 +26,48 @@ describe('1', () => {
     done()
   })
 
-  it('2', async done => {
+  it('in client test', async done => {
     const title = await page.title()
     expect(title).toBe('imagemagick-browser integration tests')
-
-    await page.waitFor(() => { return (window as   any).transformationsFinishedFromTest1 })
-    const result = await page.evaluate(()=>{
-      return document.querySelector<HTMLImageElement>('#knightImageOut').width+', '+document.querySelector<HTMLImageElement>('#knightImage').width
+    const result = await page.evaluate(() => {
+      return document.querySelector<HTMLImageElement>('#knightImageOut').width + ', ' + document.querySelector<HTMLImageElement>('#knightImage').width
     })
     expect(result).toBe('140, 100')
     done()
   })
 
+  it('evaluating from here', async done => {
 
-  // it('1', () => {
-  //   expect(1).toBe(1)
-  // })
+    const title = await page.title()
+    expect(title).toBe('imagemagick-browser integration tests')
+    let result = await page.evaluate(async () => {
+      const im = (window as any).imageMagickBrowser as IM;
 
-  // 'convert', 'People.jpg', '-clone', '0', 'People2.jpg', '-compose', 'difference', '-composite', '-threshold', '5%', '-fill', 'red', '-opaque', 'white', '-transparent', 'black', '-compose', 'over', '-composite', 'people_compare2.png'
-  ','//','convert','People.jpg','\
-  // \( -clone 0 People2.jpg -compose difference -composite \
-  // -threshold 5% -fill red -opaque white -transparent black \) \
-  // -compose over -composite people_compare2.png
+      const div = document.createElement('div')
+      div.innerHTML = `
+<p>Source image: <img src="knight.png" id="knightImage2"></p>
+<p>Result image: <img src="knightOut.png" id="knightImageOut2"></p>`
+      document.body.appendChild(div)
+
+      const inputFiles = await im.buildInputFiles(['knight.png'])
+      const result = await im.execute({
+        inputFiles,
+        commands: [['convert', 'knight.png', '-rotate', '63', 'knightOut.png']]
+      })
+      im.writeOutputImageToEl(result[0].outputFiles[0], document.getElementById('knightImageOut2') as HTMLImageElement)
 
 
+      // heads up ! - give some time to browser to load the image in the DOM bfure reading its width
+      return new Promise((resolve) => {
+        setTimeout(() => {
+          resolve(document.querySelector<HTMLImageElement>('#knightImageOut2').width + ', ' + document.querySelector<HTMLImageElement>('#knightImage2').width)
+        }, 100);
+      })
+    })
+    expect(result).toBe('136, 100')
+    done()
+  })
 
 })
+
+
--- imagemagick-browser/test-project1/src/index.ts
+++ imagemagick-browser/test-project1/src/index.ts
@@ -1,43 +1,59 @@
-import { execute, buildInputFiles, writeOutputImageToEl } from 'imagemagick-browser'
-
-// const images = [
+import * as im from 'imagemagick-browser'
+type IM = typeof im
+const w = window as any
+// const transformations = [
 //   {
-//     name: 'knight.png',
-//     url: 'knight.png'
+//     commands: [['convert', '$INPUT1', '-rotate', '33', '$OUTPUT1']]
 //   }
-// ]
-const transformations = [
-  {
-    commands: [['convert', '$INPUT1', '-rotate', '33', '$OUTPUT1']]
-  }
-];
-(window as any).transformationsFromTest1 = transformations
+// ];
+// w.transformationsFromTest1 = transformations
 
-function renderHtml() {
+// function renderHtml() {
+//   const html = `
+//   <div>
+//     <p>Source image: <img src="knight.png" id="knightImage"></p>
+//     <p>Result image: <img src="knightOut.png" id="knightImageOut"></p>
+//   </div>
+//   `
+//   const mainEl = document.createElement('div')
+//   mainEl.innerHTML = html
+//   document.body.appendChild(mainEl)
+// }
 
-  const html = `
-  <div>
-    <p>Source image: <img src="knight.png" id="knightImage"></p>
-    <p>Result image: <img src="knightOut.png" id="knightImageOut"></p>
-  </div>
-  `
-  const mainEl = document.createElement('div')
-  mainEl.innerHTML = html
-  document.body.appendChild(mainEl)
-}
-async function domagick1() {
-  const inputFiles = await buildInputFiles(['knight.png'])
-  const result = await execute({
+// async function domagick1() {
+
+
+// }
+
+// w.imEvaluate = async function(fn: (im: IM)=>void){
+//   return await fn(im)
+// }
+async function fn1(im: IM){
+  const div = document.createElement('div')
+  div.innerHTML = `
+  <p>Source image: <img src="knight.png" id="knightImage"></p>
+  <p>Result image: <img src="knightOut.png" id="knightImageOut"></p>`
+  document.body.appendChild(div)
+
+  const inputFiles = await im.buildInputFiles(['knight.png'])
+  const result = await im.execute({
     inputFiles,
     commands: [['convert', 'knight.png', '-rotate', '33', 'knightOut.png']]
   })
-  writeOutputImageToEl(result[0].outputFiles[0], document.getElementById('knightImageOut') as HTMLImageElement);
-  (window as any).transformationsFinishedFromTest1 = true
+  im.writeOutputImageToEl(result[0].outputFiles[0], document.getElementById('knightImageOut') as HTMLImageElement)    
 }
-
 async function main() {
-  renderHtml()
-  await domagick1();
-  (window as any).transformationsFinishedFromTest1 = true
+  // expose imagemagick-browser  APIs so we can evaluate expressions from node.js puppeteer
+
+  await fn1(im)
+  w.imageMagickBrowser = im
+
+
+  // console.log(document.querySelector<HTMLImageElement>('#knightImageOut').width+', '+document.querySelector<HTMLImageElement>('#knightImage').width);
+  // }, 1000);
+
+  // renderHtml()
+  // await domagick1();
+
 }
 main()
