388e0c2ef0bef643bc949f9d4266fc61bc9006b6
Sebastian Gurin <sebastigurin@gmail.com>
2018-11-24 19:44:52 -0300

    phto-morph

--- photo-morph/src/model/morphs.ts
+++ photo-morph/src/model/morphs.ts
@@ -32,20 +32,13 @@ export enum MorphTag {
   distort
 }
 
-const commonArguments = [
+const commonArguments : Argument[]= [
   {
     type: ArgumentType.number,
     id: "loop",
     name: "loop",
     description: "Set iterations to zero to repeat the animation an infinite number of times, otherwise the animation repeats itself up to iterations times.",
     defaultValue: 0
-  },
-  {
-    type: ArgumentType.number,
-    id: "frames",
-    name: "frames",
-    description: "determine how many images to interpolate between each image",
-    defaultValue: 6
   },
 ]
 
@@ -55,7 +48,7 @@ class MutateAndLint implements Morph {
   description = `mutate both images and tint with same color until both are very similar, then concatenate sequences`
   tags = [MorphTag.morph, MorphTag.animation]
   command = ""
-  arguments = [
+  arguments: Argument[] = [
     {
       type: ArgumentType.number,
       id: "swirlFactor",
@@ -63,6 +56,13 @@ class MutateAndLint implements Morph {
       description: "",
       defaultValue: 100
     },
+    {
+      type: ArgumentType.number,
+      id: "frames",
+      name: "frames",
+      description: "determine how many images to interpolate between each image",
+      defaultValue: 6
+    } as Argument
   ].concat(commonArguments)
   async template(config: MorphConfig & { arguments: Arguments & { frames: number, swirlFactor: number } }) {
     const inputFiles = await forceSameSize(config)
@@ -102,6 +102,13 @@ class ColorMorph implements Morph {
       description: "background color for the smaller image that is enlarged",
       defaultValue: "#ffffff"
     } as Argument,
+    {
+      type: ArgumentType.number,
+      id: "frames",
+      name: "frames",
+      description: "determine how many images to interpolate between each image",
+      defaultValue: 6
+    },
   ].concat(commonArguments)
 
   async template(config) {
@@ -109,27 +116,136 @@ class ColorMorph implements Morph {
     const commands = `
       convert ${inputFiles[0].name} ${inputFiles[1].name} \\
         -morph ${config.arguments.frames} \\
-        -set delay "%[fx:(t>0&&t<n-1)?10:240]" \\
+        -set delay '%[fx:(t>0&&t<n-1)?10:640]' \\
         -duplicate 1,-2-1 -loop ${config.arguments.loop} output${getUniqueId()}.gif`
     const result = await execute({ inputFiles, commands })
     return result.outputFiles
   }
+}
+
+
+class ComposeMorph implements Morph {
+  name = "compose"
+  id = "composeMorph"
+  description = `http://www.imagemagick.org/Usage/compose/#dissolve`
+  tags = [MorphTag.morph, MorphTag.animation]
+  command = ""
+  arguments: Argument[] = [
+    {
+    type: ArgumentType.color,
+    id: "backgroundColor",
+    name: "Background Color",
+    description: "background color for the smaller image that is enlarged",
+    defaultValue: "#ffffff"
+  } ,
+    {
+      type: ArgumentType.selectOne,
+      id: "method",
+      name: "Method",
+      list: [{id: "dissolve", name: "dissolve"}, {id: "blend", name: "blend"} ],
+      description: "compose method",
+      defaultValue: "dissolve"
+    } as Argument,
+  ].concat(commonArguments)
+
+  async template(config) {
+    const inputFiles = await forceSameSize({...config, backgroundColor: config.arguments.backgroundColor})
+    const list = seq(1, 1, 10).map(i=>i*10)
+    const commands = `
+    convert ${inputFiles[0].name} f0.miff
+    convert ${inputFiles[1].name} f1.miff
+    ${list.map(i=>
+      `convert f0.miff f1.miff -alpha on \\
+    -compose ${config.arguments.method}  -define compose:args=${i} \\
+    -gravity South  -composite     compose_output_${i}.miff`).join("\n")}
+    convert -delay 50 f0.miff -morph 4 -delay 20 ${list.map(i=>`compose_output_${i}.miff`).join(" ")} -delay 200 f1.miff -delay 20 ${list.map(i=>`compose_output_${i}.miff`).reverse().join(" ")} output.gif
+    `
+    const result = await execute({ inputFiles, commands })
+    return [result.outputFiles[result.outputFiles.length-1]]
+  }
 
 }
+
+
+
+class PixelatedMorph implements Morph {
+  name = "pixelated"
+  id = "pixelated "
+  description = `http://www.imagemagick.org/Usage/transform/#pixelate`
+  tags = [MorphTag.morph, MorphTag.animation]
+  command = ""
+  arguments: Argument[] = [
+    {
+    type: ArgumentType.color,
+    id: "backgroundColor",
+    name: "Background Color",
+    description: "background color for the smaller image that is enlarged",
+    defaultValue: "#ffffff"
+  } ,
+  {
+  type: ArgumentType.number,
+  id: "count",
+  name: "Pixelation amount",
+  description: "The larger, the more pixelated the images will be in the animation",
+  defaultValue: 10
+} ,
+    {
+      type: ArgumentType.selectOne,
+      id: "method",
+      name: "Method",
+      list: [{id: "sample", name: "sample"}, {id: "scale", name: "scale"}, {id: "resize", name: "resize"} ],
+      description: "compose method",
+      defaultValue: "sample"
+    } as Argument,
+  ].concat(commonArguments)
+
+  async template(config) {
+    const inputFiles = await forceSameSize({...config, backgroundColor: config.arguments.backgroundColor})
+    const list = seq(1, 1, config.arguments.count).map(n=>60/(n*2))
+    const newSize = `${config.inputFiles[0].info.image.geometry.width}x${config.inputFiles[0].info.image.geometry.height}`
+    // convert f0.miff  -sample 25%  -scale 70x46\!  
+    const commands = `
+    convert ${inputFiles[0].name} f0.miff
+    convert ${inputFiles[1].name} f1.miff
+    ${list.map((n, i)=>
+      `convert f0.miff -${config.arguments.method} ${n}% -scale ${newSize}! f0_${i}.miff
+      convert f1.miff -${config.arguments.method} ${n}% -scale ${newSize}! f1_${i}.miff`).join("\n")}
+    convert -delay 50 f0.miff -morph 2 -delay 20 ${list.map((n, i)=>`f0_${i}.miff`).join(" ")} ${list.map((n, i)=>`f1_${i}.miff`).reverse().join(" ")} -delay 200 f1.miff   output.gif
+    `
+    console.log(commands)
+    
+    const result = await execute({ inputFiles, commands })
+    return [result.outputFiles[result.outputFiles.length-1]]
+  }
+
+}
+
+
+
+
 
 export const morphs: Morph[] = [
 
   new MutateAndLint(),
   new ColorMorph(),
+  new ComposeMorph(),
+  new PixelatedMorph(),
 
   {
     name: "morph resize",
     id: "resizeMorph",
-    description: `https://www.imagemagick.org/Usage/anim_mods/#morph_resize`,
+    description: `https:// www.imagemagick.org/Usage/anim_mods/#morph_resize`,
     tags: [MorphTag.morph, MorphTag.animation],
     // arguments: commonArguments,
     command: "",
     arguments: [
+      {
+        type: ArgumentType.number,
+        id: "frames",
+        name: "frames",
+        description: "determine how many images to interpolate between each image",
+        defaultValue: 6
+       } as Argument
     ].concat(commonArguments),
 
     async template(config) {
@@ -141,7 +257,7 @@ export const morphs: Morph[] = [
       -set delay '%[fx:(t>0&&t<n-1)?10:60]' \\
       -duplicate 1,-2-1  -loop ${config.arguments.loop}  output${getUniqueId()}.gif`
       const result = await execute({ inputFiles, commands })
-      return result.outputFiles
+      return [result.outputFiles[result.outputFiles.length-1]]
     }
   },
   {
@@ -160,18 +276,18 @@ export const morphs: Morph[] = [
       } as Argument,
       {
         type: ArgumentType.number,
-        id: "speedX",
-        name: "Speed X",
+        id: "cropSpeed",
+        name: "Crop Speed",
         description: "Horizontal crop speed",
         defaultValue: 9
       } as Argument,
-
       {
-        type: ArgumentType.number,
-        id: "speedY",
-        name: "Speed Y",
-        description: "vertical crop speed",
-        defaultValue: 0
+        type: ArgumentType.selectOne,
+        id: "direction",
+        name: "Direction",
+        list: [{id: "vertical", name: "vertical"}, {id: "horizontal", name: "horizontal"}, {id: "both", name: "both"}],
+        description: "crop direction",
+        defaultValue: "horizontal"
       } as Argument,
     ].concat(commonArguments),
 
@@ -180,7 +296,7 @@ export const morphs: Morph[] = [
       const commands = `
       convert  \\
       ${inputFiles[0].name} ${inputFiles[1].name} \\
-      \( -clone 0 -crop ${config.arguments.speedX}x${config.arguments.speedY} \) \\
+      \( -clone 0 -crop ${config.arguments.direction!=="vertical" ? config.arguments.cropSpeed : 0}x${config.arguments.direction!=="horizontal" ? config.arguments.cropSpeed : 0} \) \\
       -set delay 10  -loop ${config.arguments.loop} output${getUniqueId()}.gif`
       const result = await execute({ inputFiles, commands })
       return result.outputFiles
